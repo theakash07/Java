9b944a9499153b584b74ef39b3d811dbdd9bfade
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-08-03 01:54:57 -0700

    FIRST JAVA PROGRAM

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+class Simple {  
+  //public is a access specifier thath means we can access it from anywhere in the program
+  //static means function of this class doesn't need a object to run it static keyword gives it power to run without any object of this class.
+  // void is nothing but it's returning nothing that's why using return type.
+  //main is default function of this class .
+  // System.out is not a class but a reference to a PrintStream object, and it allows you to print text to the console in Java.
+  //it's a static field of the java.lang.system class.
+  //println is printstream which prints the output and newline as well..
+  //you can use printf as well for formatting just like c++.
+    public static void main(String args[]) {  
+        System.out.println("The sum of program");  
+        
+    }  
+}  
