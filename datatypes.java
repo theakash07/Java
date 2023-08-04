2819f2a71653de12675cbc21dfc9b8dd5f6ce8bd
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-08-04 01:42:15 -0700

    Data types of java

---
+++ datatypes.java
@@ -0,0 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile, and run your Java code online
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        byte data1 = 21;
+        int data2 = 22;
+        short data3 = 23;
+        long data4 = 23235425L;
+        float data5 = 23.54f; // Note: Floating-point literals require an 'f' suffix
+        double data6 = 2334.09;
+        boolean data7 = true; // Note: 'bool' should be 'boolean' and 'True' should be 'true'
+        char data8 = 'a';
+    }
+}
